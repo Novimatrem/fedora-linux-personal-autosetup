c82e77c9105645f3203a4e278d6b6785d622755f
Novimatrem <TheNovimatrem@protonmail.ch>
2022-03-18 22:35:09 +0000

    v0.5

--- run-autosetup.sh
+++ run-autosetup.sh
@@ -68,7 +68,7 @@ alias flatpakins="flatpak install flathub $1 -y --noninteractive"
 
 alias fullupdate="sudo dnf autoremove -y && sudo dnf check -y && sudo dnf upgrade -y && sudo dnf distro-sync -y && sudo dnf autoremove -y && sudo dnf check -y"
 
-alias gtfo="zenity --warning --text 'Process complete. Shutting down in 1 minute.' & shutdown -h +1"
+alias gtfo="zenity --warning --text 'Process complete. Shutting down in 1 minute. REFER TO NOTES.TXT IN HOME AFTER REBOOT.' & shutdown -h +1"
 
 alias saybox='zenity --warning --text $1'
 
@@ -224,6 +224,14 @@ sudo dnf autoremove -y && sudo dnf check -y && sudo dnf upgrade -y && sudo dnf d
 # Install some deps
 dnfins wget
 dnfins curl
+dnfins xdotool
+dnfins zenity
+dnfins wget
+dnfins sox
+dnfins xbindkeys
+dnfins ffmpeg
+dnfins x11-utils
+dnfins wmctrl
 #-/
 #/
 
@@ -236,4 +244,222 @@ flatpakins org.gnome.Epiphany
 #-/
 #/
 
+fullupdate
 
+# Set up manual notes, drop a few.
+touch /home/$(whoami)/NOTES.txt
+
+lvnote "Notes: (to-do, AKA things I am yet to automate)-"
+
+lvnote "Install the MVPS HOSTS file with this script https://gitlab.com/-/snippets/1992890"
+
+lvnote "Follow your blog post to fully permanently disable mouse acceleration. https://novimatrem.gitlab.io/blog/2020/08/22/how-to-fully-properly-disable-mouse-acceleration-in-most-linux-distros-and-de.html "
+
+# Ensure the system doesn't fall asleep in any way, to the best of my ability, that would be bad to occur during this.
+sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
+gsettings set org.gnome.desktop.session idle-delay 0
+gsettings set org.gnome.settings-daemon.plugins.power sleep-display-ac 0
+gsettings set org.gnome.settings-daemon.plugins.power sleep-display-battery 0
+gsettings set org.gnome.desktop.session idle-delay 0
+sudo gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type 'nothing'
+sudo gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-timeout 0
+sudo setterm -blank 0 -powersave off -powerdown 0
+sudo xset s 0 0
+sudo xset dpms 0 0
+sudo xset s off
+xset -dpms
+xset s noblank
+xset s off
+gsettings set org.gnome.settings-daemon.plugins.power active false
+gsettings set org.gnome.desktop.screensaver idle-activation-enabled false
+gsettings set org.gnome.settings-daemon.plugins.power idle-dim false
+setterm -blank 0
+setterm -blank 0 -powerdown 0
+sudo chown $USER /etc/issue
+sudo echo -ne "\033[9;0]" >> /etc/issue
+gsettings set org.gnome.desktop.session idle-delay 0
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type 'nothing'
+gsettings set org.gnome.settings-daemon.plugins.power idle-dim false
+gsettings set org.gnome.settings-daemon.plugins.power ambient-enabled false
+#-/
+#/
+
+# Create and own the /opt folder, because I use it very frequently.
+sudo mkdir /opt
+cd /opt/
+sudo chown $USER /opt
+sudo chown $USER /opt/*
+#-/
+#/
+
+# Install some snaps I use or depend upon.
+snapins snapd
+snapins hello-world
+snapins snap-store
+snapins languagetool
+snapins pngcrush --edge
+snapins tetris-thefenriswolf
+snapins telegram-desktop
+sudo snap refresh
+#-/
+#/
+
+# Install some flatpaks I use or depend upon.
+flatpakins com.adobe.Flash-Player-Projector
+flatpakins com.zandronum.Zandronum
+flatpakins com.eduke32.EDuke32
+flatpakins net.sourceforge.fretsonfire
+flatpakins org.zdoom.GZDoom
+flatpakins org.ppsspp.PPSSPP
+flatpakins org.zdoom.GZDoom
+flatpak update
+#-/
+#/
+
+# Install variety so that my wallpaper doesn't stay the same for too long.
+dnfins variety
+#/
+
+# Various tools to improve performance
+dnfins gamemode
+dnfins cpufrequtils
+dnfins preload
+sudo systemctl disable ondemand
+#-/
+#/
+
+# Package to avoid hard crashing when running out of RAM, because Linux's OOM-killer sucks.
+dnfins earlyoom
+sudo systemctl enable earlyoom
+sudo systemctl start earlyoom
+sleep 0s && nohup earlyoom && rm -rf $HOME/nohup.out && rm -rf $(pwd)/nohup.out && rm -rf /opt/nohup.out && disown & disown && echo ""
+echo ""
+#-/
+#/
+
+# Make some folders that I want to frequently use.
+mkdir /home/$(whoami)/Videomaking
+mkdir /home/$(whoami)/Working
+mkdir /home/$(whoami)/Working/Important
+mkdir /home/$(whoami)/Applications
+mkdir /home/$(whoami)/DnD
+mkdir /home/$(whoami)/Flashes
+mkdir /home/$(whoami)/EMU Roms
+mkdir /home/$(whoami)/.icons
+mkdir /home/$(whoami)/Pictures/Wallpapers
+#-/
+#/
+
+# Scanner support
+dnfins skanlite
+#/
+
+# Install osu!lazer
+mkdir /home/$(whoami)/Applications
+cd /home/$(whoami)/Applications
+dnfins wget
+wget https://github.com/ppy/osu/releases/latest/download/osu.AppImage
+sudo chmod +x ./osu.AppImage
+#-/
+#/
+
+# Make Xfce4 window buttons on the left and as I like them
+xfconf-query -c xfwm4 -p /general/button_layout -s "CHM|O"
+#/
+
+# Some disk tools
+dnfins gnome-disk-utility
+dnfins gnome-disks
+dnfins gparted
+#-/
+#/
+
+# redshift for eye protection
+dnfins redshift-gtk
+#/
+
+# toot cli fedi client
+dnfins toot
+#/
+
+# IDLE, for Python
+dnfins idle
+dnfins idle3
+#-/
+#/
+
+# More browsers/terminal usability
+dnfins lynx
+dnfins links
+dnfins w3m
+dnfins w3m-img
+dnfins links
+dnfins links2
+dnfins elinks
+dnfins byobu
+dnfins tmux
+snapins dm-tools
+#-/
+#/
+
+# NoteBot (by adolfintel / fdossena) install+autorun
+dnfins wget
+
+cd /opt
+wget https://downloads.fdossena.com/Projects/StickyNotes/notebot-1.6-bin.7z
+7z x notebot-1.6-bin.7z
+
+sleep 0s && nohup java -jar "/opt/StickyNotes.jar" && rm -rf $HOME/nohup.out && rm -rf $(pwd)/nohup.out && rm -rf /opt/nohup.out && disown & disown
+
+#
+# CREATE STARTUP PROGRAM ENTRY
+#
+mkdir /home/$(whoami)/.config/autostart
+touch /home/$(whoami)/.config/autostart/notebot.desktop
+
+echo "[Desktop Entry]" >> /home/$(whoami)/.config/autostart/notebot.desktop
+echo "Type=Application" >> /home/$(whoami)/.config/autostart/notebot.desktop
+echo "Name=notebot" >> /home/$(whoami)/.config/autostart/notebot.desktop
+echo "Exec=java -jar /opt/StickyNotes.jar" >> /home/$(whoami)/.config/autostart/notebot.desktop
+echo "Comment=notebot" >> /home/$(whoami)/.config/autostart/notebot.desktop
+echo "Terminal=false" >> /home/$(whoami)/.config/autostart/notebot.desktop
+
+echo ""
+echo ""
+echo "verify below:"
+echo ""
+ls /home/$(whoami)/.config/autostart/
+echo ""
+cat /home/$(whoami)/.config/autostart/notebot.desktop
+echo ""
+echo "done listing"
+echo ""
+echo ""
+#
+# /END OF CREATE STARTUP PROGRAM ENTRY
+#
+# /NoteBot (by adolfintel / fdossena) install+autorun
+
+# Disable the Xfce4/Xfwm4 compositor because it's very input lag and meh
+xfconf-query -c xfwm4 -p /general/use_compositing -s false
+#/
+
+# Text editors
+dnfins kwrite
+dnfins gedit
+#-/
+#/
+
+# Cleanup
+rm -rf /opt/nohup.out
+rm -rf $HOME/nohup.out
+rm -rf $(pwd)/nohup.out
+rm -rf /home/$(whoami)/fast.sh
+rm -rf /home/$(whoami)/Desktop/fast.sh
+sudo chown $USER /opt/
+sudo chown $USER /opt/*
+rm -rf /opt/nohup.out
+sudo rm -rf /opt/nohup.out
+#-/
+#/
