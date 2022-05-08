c40fbe278bd65f459ac96902f5d1149c286ce3fd
Novimatrem <thenovimatrem@protonmail.ch>
2022-05-08 16:33:36 +0000

    install MATE

--- run-autosetup.sh
+++ run-autosetup.sh
@@ -223,6 +223,44 @@ dnfins wmctrl
 #-/
 #/
 
+# Install MATE Desktop on Fedora v3
+
+# The MATE theme I use is known as GreenLaguna, it's not incredibly pretty, but, it means that
+#  titlebar size, height, and the location, size, and position of the window buttons stays as
+#   consistent as possible between apps/windowing toolkits, as this is very important to me,
+#    and was the reason I can't use Xfce 4.16, due to how it treats this with GTK vs Non-GTK apps.
+
+echo "Working..."
+
+sudo dnf autoremove -y && sudo dnf check -y && sudo dnf upgrade -y && sudo dnf distro-sync -y && sudo dnf autoremove -y && sudo dnf check -y
+
+sudo dnf update -y
+sudo dnf upgrade -y
+sudo dnf upgrade --refresh -y
+
+sudo dnf install -y sddm
+sudo systemctl enable sddm
+
+sudo dnf install -y mate-desktop
+sudo dnf install -y @mate-desktop
+sudo dnf groupinstall mate-desktop
+sudo dnf -y group install "MATE Desktop"
+sudo dnf install -y mate-desktop-environment
+
+# MATE applications
+sudo dnf install -y @mate-applications
+sudo dnf groupinstall mate-applications
+#-/
+#/
+
+sudo dnf update -y
+sudo dnf upgrade -y
+sudo dnf upgrade --refresh -y
+
+sudo dnf autoremove -y && sudo dnf check -y && sudo dnf upgrade -y && sudo dnf distro-sync -y && sudo dnf autoremove -y && sudo dnf check -y
+#-/
+#/
+
 # Install browsers
 dnfins firefox
 dnfins chromium
