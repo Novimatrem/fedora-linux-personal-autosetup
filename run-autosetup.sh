e242be0558085c5d88a70d74cf4775f898501245
Novimatrem <thenovimatrem@protonmail.ch>
2022-08-23 11:12:53 +0000

    flatpak gajim and plugins

--- run-autosetup.sh
+++ run-autosetup.sh
@@ -1008,3 +1008,11 @@ dnfins powertop
 # atlauncher flatpak
 flatpakins com.atlauncher.ATLauncher
 #/
+
+# flatpak gajim and plugins
+flatpak install org.gajim.Gajim -y --noninteractive
+flatpak install org.gajim.Gajim.Plugin.appindicator -y --noninteractive
+flatpak install org.gajim.Gajim.Plugin.url_image_preview -y --noninteractive
+flatpak install org.gajim.Gajim.Plugin.omemo -y --noninteractive
+# ^ flatpak gajim and plugins /> 
+
