524b4990e3de9d0014371db4080e1470c36ce421
Novimatrem <thenovimatrem@protonmail.ch>
2022-12-13 09:11:30 +0000

    extra flatpak ensure

--- run-autosetup.sh
+++ run-autosetup.sh
@@ -1390,3 +1390,9 @@ dnfins flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpakins sh.ppy.osu
 #/
+
+# Make flatpaks good and working
+dnfins flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+#/
+
